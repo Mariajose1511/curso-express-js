21fd86f6f53e55e7d9424afa1646ceca053ec535
juliodevs <juliocesar8881@hotmail.com>
2025-12-22 07:33:18 -0500

    Configuración de body-parser

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 const express = require('express');
-const app = express();
 const PORT = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send(`
@@ -10,6 +14,23 @@ app.get('/', (req, res) => {
     <p>corre en el puerto: ${PORT}</p>
   `);
 });
+
+app.get('/users/:id', (req,res) => {
+  const userId = req.params.id;
+  res.send(`Usuario con ID: ${userId}`);
+});
+
+app.get('/search',(req,res) => {
+  const terms = req.query.termino || 'No se proporcionó término de búsqueda';
+  const category = req.query.categoria || 'No se proporcionó categoría';
+  res.send(`
+    <h2>Resultados de búsqueda</h2>
+    <p>Término: ${terms}</p>
+    <p>Categoría: ${category}</p>
+  `);
+  });
+
+
 
 app.listen(PORT, () => {
   console.log(`La aplicación está escuchando en http://localhost:${PORT}`);
